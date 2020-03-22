373a3e638f9c1a29b091e1f7662246d7edcb7b5e
Robin Tan <webtjw@foxmail.com>
2020-03-22 22:21:50 +0800

    增加注释

--- src/core/instance/render-helpers/resolve-slots.js
+++ src/core/instance/render-helpers/resolve-slots.js
@@ -9,17 +9,21 @@ export function resolveSlots (
   children: ?Array<VNode>,
   context: ?Component
 ): { [key: string]: Array<VNode> } {
+  /* slot 就是 children 的形式 */
   if (!children || !children.length) {
     return {}
   }
   const slots = {}
   for (let i = 0, l = children.length; i < l; i++) {
     const child = children[i]
+    /* TODO child.data 是什么数据 */
     const data = child.data
+    /* TODO 不理解。如果当前节点是 slot 节点，则移除其 slot 属性？ */
     // remove slot attribute if the node is resolved as a Vue slot node
     if (data && data.attrs && data.attrs.slot) {
       delete data.attrs.slot
     }
+    /* TODO 不理解。只有在与 vnode 是同一 context 下时，具名 slot 才会 */
     // named slots should only be respected if the vnode was rendered in the
     // same context.
     if ((child.context === context || child.fnContext === context) &&
