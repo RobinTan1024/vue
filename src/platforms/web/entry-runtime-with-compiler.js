6fcf1293f885d922dd44a13651a4f4a606796865
Robin Tan <webtjw@foxmail.com>
2020-04-14 23:34:54 +0800

    更新 mount 方法注释（编译模板待阅）

--- src/platforms/web/entry-runtime-with-compiler.js
+++ src/platforms/web/entry-runtime-with-compiler.js
@@ -21,6 +21,7 @@ Vue.prototype.$mount = function (
 ): Component {
   el = el && query(el)
 
+  /* 不能把组件挂载在 html&body 下 */
   /* istanbul ignore if */
   if (el === document.body || el === document.documentElement) {
     process.env.NODE_ENV !== 'production' && warn(
@@ -29,12 +30,20 @@ Vue.prototype.$mount = function (
     return this
   }
 
+  /**
+   * el = 提供一个在页面上已存在的 DOM 元素作为 Vue 实例的挂载目标，可以是 CSS 选择器，也可以是一个 HTMLElement 实例
+   * template = 一个字符串模板作为 Vue 实例的标识使用，模板将会替换挂载的元素（only with cimpiler）
+   * render = 字符串模板的代替方案，允许你发挥 JavaScript 最大的编程能力。该渲染函数接收一个 createElement 方法作为第一个参数用来创建 VNode
+   */
   const options = this.$options
+  // 处理 template/el 并转换为渲染函数
   // resolve template/el and convert to render function
   if (!options.render) {
+    // 得到要渲染的模板 template
     let template = options.template
     if (template) {
       if (typeof template === 'string') {
+        // 把指定 id 的 DOM 内容赋值给 template
         if (template.charAt(0) === '#') {
           template = idToTemplate(template)
           /* istanbul ignore if */
@@ -57,11 +66,13 @@ Vue.prototype.$mount = function (
       template = getOuterHTML(el)
     }
     if (template) {
+      // 性能监测：编译字符串模板的开始
       /* istanbul ignore if */
       if (process.env.NODE_ENV !== 'production' && config.performance && mark) {
         mark('compile')
       }
 
+      // 编译过程
       const { render, staticRenderFns } = compileToFunctions(template, {
         outputSourceRange: process.env.NODE_ENV !== 'production',
         shouldDecodeNewlines,
@@ -69,9 +80,11 @@ Vue.prototype.$mount = function (
         delimiters: options.delimiters,
         comments: options.comments
       }, this)
+      // 得到了渲染函数
       options.render = render
       options.staticRenderFns = staticRenderFns
 
+      // 性能监测：编译字符串模板的结束
       /* istanbul ignore if */
       if (process.env.NODE_ENV !== 'production' && config.performance && mark) {
         mark('compile end')
